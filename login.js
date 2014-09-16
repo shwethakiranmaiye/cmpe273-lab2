d8546336436b8cd845b161e1e4891dd77b463de8
ravi <ravi@midastechllc.com>
2014-09-16 13:48:35 -0700

    Added code in place of TODO for the lab2 assignment

--- login.js
+++ login.js
@@ -37,14 +37,33 @@ Login.prototype.login = function(_name, _email) {
 	return sessionId;
 };
 
+/**refresh session id */
+Login.prototype.refresh = function(sid) {
+   /*
+    * Generate unique session id and set it into sessionMap like foo@bar.com
+    */
+    if(sid in this.sessionMap){
+        var my_value = this.sessionMap[sid];
+        delete this.sessionMap[sid]; 
+        var sessionId = new Date().getTime();
+        this.sessionMap[sessionId] = my_value;
+        console.log('new session id ' + sessionId );
+        return sessionId;
+    }else{
+        return null;
+    }
+};
+
 /**
  * Logout from the server
  */ 
 Login.prototype.logout = function(sessionId) {
-	console.log('logout::' + sessionId);
-   /*
-	* TODO: Remove the given sessionId from the sessionMap
-	*/
+    if(sessionId in this.sessionMap){
+        delete this.sessionMap[sessionId];
+        console.log('logout::' + sessionId);
+        return true;
+    }
+    return false;
 };
 
 // Export the Login class
